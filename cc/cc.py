a3d23de832bf6946f6c5d110dcf7c8c247b845c2
Ash <124933859+Ashisaphnx@users.noreply.github.com>
2023-05-15 13:24:29 -0400

    Fixed the COUNT variable

--- cc/cc.py
+++ cc/cc.py
@@ -3,43 +3,33 @@ COUNT = 0
 CARDS_ON_TABLE = 0
 MAX_CARDS = 208
 print ("Hi")
+
 def ask_card():
-    '''Will ask for the avalible cards on the table'''
+    '''Will ask for the available cards on the table'''
     card = input("What is your card: ")
     print (card)
-    if card == "A":
-        count = count +0
-    elif card == "J":
-        count = count -2
-    elif card == "Q":
-        count = count -2
-    elif card == "K":
-        count = count -2
-    elif card == "10":
-        count = count -2
-    elif card == "2":
-        count = count +1
-    elif card == "3":
-        count = count +1
-    elif card == "4":
-        count = count +2
-    elif card == "5":
-        count = count +2
-    elif card == "6":
-        count = count +2
-    elif card == "7":
-        count = count +1
-    elif card == "8":
-        count = count +0
-    elif card == "9":
-        count = count -1
-    else :
-        count = 0
-    print (count)
-    true_count = count / 4
-    if count >=4:
-        print (true_count)
+    try:
+        card_value = {
+            "A": 1,
+            "2": 1,
+            "3": 1,
+            "4": 1,
+            "5": 1,
+            "6": 1,
+            "7": 0,
+            "8": 0,
+            "9": 0,
+            "10": -1,
+            "J": -1,
+            "Q": -1,
+            "K": -1
+        }[card]
+    except KeyError:
+        print("Invalid card")
+        return
 
-while CARDS_ON_TABLE <= MAX_CARDS:
-    ask_card()
-    CARDS_ON_TABLE = CARDS_ON_TABLE +1
+    COUNT += card_value
+    print (COUNT)
+    true_count = COUNT / 4
+    if COUNT >=4:
+        print (true_count)
